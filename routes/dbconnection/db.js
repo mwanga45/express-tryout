c5696368079235b416626c5970ef7105de5e588e
Lynxprazo <issamwanga02@gmail.com>
2025-04-23 01:11:23 +0300

    more changes

--- routes/dbconnection/db.js
+++ routes/dbconnection/db.js
@@ -26,13 +26,31 @@ async function ensureDatabaseExist(){
         // 3) If not, create it
         console.log(`Creating database "${PG_DATABASE}"…`);
         await AdiminClient.query(`CREATE DATABASE "${database}"`);
-        console.log(`Database "${PG_DATABASE}" created.`);
+        console.log(`Database "${database}" created.`);
       } else {
-        console.log(`Database "${PG_DATABASE}" already exists.`);
+        console.log(`Database "${database}" already exists.`);
       }
     
       await AdiminClient.end();
 }
+async function initPool(){
+    await ensureDatabaseExist()
+    const Poolconnection  = new Pool({
+        host:host,
+        port:port,
+        user:user,
+        password:password,
+        database:database,
+        max:20,
+        idleTimeoutMillis:3000
+    });
+    const { rows } = await pool.query('SELECT NOW()');
+    console.log('Connected to database at', rows[0].now);
+  
+    return Pool;
+
+}
+module.exports = initPool()
 
 
 
