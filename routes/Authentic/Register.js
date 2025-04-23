1f9868a06fcfd703e42b6c593fdbc903248ad6ff
Lynxprazo <issamwanga02@gmail.com>
2025-04-23 13:34:31 +0300

    more changes

--- routes/Authentic/Register.js
+++ routes/Authentic/Register.js
@@ -1,28 +1,36 @@
-require('dotenv').config()
-var Pool = require("../dbconnection/db")
-var {body, validateResult} = require("express-validator")
-var bycrpt = require("bcrypt")
-const router = require("express")
+require("dotenv").config();
+var Pool = require("../dbconnection/db");
+var { body, validateResult } = require("express-validator");
+var bycrpt = require("bcrypt");
+const router = require("express");
 
-router.post("/register",[
-body('firstname').notEmpty().withMessage("firstname is required"),
-body('secondname').notEmpty().withMessage("secondname is required "),
-body('email').isEmail().withMessage("Please Enter valid email format"),
-body("password").isLength({min: 6}).withMessage("Please Enter password with minimum of six character")
-
-], async(req,res)=>{
-    const error = validateResult(req)
-    if (!error.isEmpty() ){
-        return res.status(404).json({error : error.array()})
+router.post(
+  "/register",
+  [
+    body("firstname").notEmpty().withMessage("firstname is required"),
+    body("secondname").notEmpty().withMessage("secondname is required "),
+    body("email").isEmail().withMessage("Please Enter valid email format"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Please Enter password with minimum of six character"),
+  ],
+  async (req, res) => {
+    const error = validateResult(req);
+    if (!error.isEmpty()) {
+      return res.status(404).json({ error: error.array() });
     }
-    const {firstname, secondname, email , password} = req.body;
+    const { firstname, secondname, email, password } = req.body;
 
-    try{
-        
-
-    }catch(err){
-        console.error("Something went wrong in server", err)
+    try {
+      let query = "SELECT 1 FROM users  WHERE email = $1";
+      const { rows: existing } = (await Pool).query(query, [email]);
+      if (existing.lenght > 0){
+        return res.status(500).json({message:"Please that email is already been used"})
+      }
+      
+      const result = (await Pool).query("INSERT INTO 'users'(firstname, secondname, email, password) VALUES($1,$2,3$,4$)")
+    } catch (err) {
+      console.error("Something went wrong in server", err);
     }
-
-
-})
+  }
+);
