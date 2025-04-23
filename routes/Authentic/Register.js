fa47cb672d0e45ba3eba4ac98ab35e1fe55cb71a
Lynxprazo <issamwanga02@gmail.com>
2025-04-23 13:22:17 +0300

    more changes

--- routes/Authentic/Register.js
+++ routes/Authentic/Register.js
@@ -11,5 +11,18 @@ body('email').isEmail().withMessage("Please Enter valid email format"),
 body("password").isLength({min: 6}).withMessage("Please Enter password with minimum of six character")
 
 ], async(req,res)=>{
+    const error = validateResult(req)
+    if (!error.isEmpty() ){
+        return res.status(404).json({error : error.array()})
+    }
+    const {firstname, secondname, email , password} = req.body;
+
+    try{
+        
+
+    }catch(err){
+        console.error("Something went wrong in server", err)
+    }
+
 
 })
