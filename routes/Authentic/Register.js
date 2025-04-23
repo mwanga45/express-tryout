463b48330888c9d50c88b47628368e3108084ea8
Lynxprazo <issamwanga02@gmail.com>
2025-04-23 13:16:33 +0300

    more changes

--- routes/Authentic/Register.js
+++ routes/Authentic/Register.js
@@ -1,58 +1,15 @@
-var express = require("express");
-var router = express();
-var bycrpt = require("bcrypt");
-const connection = require("../dbconnection/db");
-const { body, validationResult } = require("express-validator");
-const Registeruser = router.post(
-  "/register",
-  [
-    body("firstname").notEmpty().withMessage("Please firstname required"),
-    body("lastname").notEmpty().withMessage("Please field lastname"),
-    body("email").isEmail().withMessage("Please Enter correct email format"),
-  ],
-  async (req, res) => {
-    const error = validationResult(req);
-    if (!error.isEmpty()) {
-      return res.status(400).json({ errors: error.array() });
-    }
-    try {
-      const checkuseremail = await body.findOne({ message: body.email });
-      let query = "SELECT email from User WHERE email = $1";
-      let isEmail = Boolean;
-      let err = (await connection)
-        .queryRow(query, checkuseremail)
-        .Scan(isEmail);
-      if (err !== null) {
-        return res.status(404).json({ message: "Something wrong Server", err });
-      }
-      if (isEmail) {
-        return res
-          .status(500)
-          .json({
-            message: "Try to use another email That one is already exist ",
-          });
-      } else {
-        try {
-          let salt = await bycrpt.genSalt(10);
-          let hashpass = await bycrpt.hash(req.body.password, salt);
-          const user = new body({
-            fistname: req.body.firstname,
-            secondname: req.body.secondname,
-            password: hashpass,
-            email: req.body.email,
-          });
-        } catch (err) {
-          console.error("Server Mallfunction", err);
-        }
-      }
-    } catch (err) {
-      console.error("Somethig went wrong", err);
-    }
-    const query =
-      "INSERT INTO User (firstname, lastname , password, email) VALUE($1, $2, $3, $4)";
-    //   var  err  =
-    return res.status(200).json({ message: "Successfully registered" });
-  }
-);
+require('dotenv').config()
+var Pool = require("../dbconnection/db")
+var {body, validateResult} = require("express-validator")
+var bycrpt = require("bcrypt")
+const router = require("express")
 
-module.exports(Registeruser);
+router.post("/register",[
+body('firstname').notEmpty().withMessage("firstname is required"),
+body('secondname').notEmpty().withMessage("secondname is required "),
+body('email').isEmail().withMessage("Please Enter valid email format"),
+body("password").isLength({min: 6}).withMessage("Please Enter password with minimum of six character")
+
+], async(req,res)=>{
+
+})
