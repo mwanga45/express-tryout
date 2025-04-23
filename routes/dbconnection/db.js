908b7a4efef1b69d2a3ca4322076d2afee0db2d2
Lynxprazo <issamwanga02@gmail.com>
2025-04-23 10:09:21 +0300

    more changes

--- routes/dbconnection/db.js
+++ routes/dbconnection/db.js
@@ -1,56 +1,51 @@
-require("dotenv").config()
-const {Pool,Client} = require("pg")
-const{
-    host ,
-    port ,
-    user ,
-    password, 
-    database  
-} =process.env
+require('dotenv').config();
+const { Pool, Client } = require('pg');
+const { host, port, user, password, database } = process.env;
 
-async function ensureDatabaseExist(){
-    const AdiminClient = new Client({
-        host: host,
-        port:port,
-        user:user,
-        password:password,
-        database :database
-    });
-    await AdiminClient.connect();
-    const { rowCount } = await AdiminClient.query(
-        `SELECT 1 FROM pg_database WHERE datname = $1`,
-        [database]
-      );
-    
-      if (rowCount === 0) {
-        // 3) If not, create it
-        console.log(`Creating database "${PG_DATABASE}"…`);
-        await AdiminClient.query(`CREATE DATABASE "${database}"`);
-        console.log(`Database "${database}" created.`);
-      } else {
-        console.log(`Database "${database}" already exists.`);
-      }
-    
-      await AdiminClient.end();
+async function ensureDatabaseExists() {
+  const adminClient = new Client({
+    host,
+    port,
+    user,
+    password,
+    database: 'postgres',
+  });
+
+  await adminClient.connect();
+  const { rowCount } = await adminClient.query(
+    'SELECT 1 FROM pg_database WHERE datname = $1',
+    [database]
+  );
+
+  if (rowCount === 0) {
+    console.log(`Creating database "${database}"…`);
+    await adminClient.query(`CREATE DATABASE "${database}"`);
+    console.log(`Database "${database}" created.`);
+  } else {
+    console.log(`Database "${database}" already exists.`);
+  }
+
+  await adminClient.end();
 }
-async function initPool(){
-    await ensureDatabaseExist()
-    const Poolconnection  = new Pool({
-        host:host,
-        port:port,
-        user:user,
-        password:password,
-        database:database,
-        max:20,
-        idleTimeoutMillis:3000
-    });
-    const { rows } = await pool.query('SELECT NOW()');
-    console.log('Connected to database at', rows[0].now);
-  
-    return Pool;
 
+async function initPool() {
+  await ensureDatabaseExists();
+
+  const pool = new Pool({
+    host,
+    port,
+    user,
+    password,
+    database,
+    max: 20,               
+    idleTimeoutMillis: 3000 
+  });
+
+
+  const { rows } = await pool.query('SELECT NOW()');
+  console.log('Connected to database at', rows[0].now);
+
+  return pool;
 }
-module.exports = initPool()
 
-
-
+module.exports = initPool();
